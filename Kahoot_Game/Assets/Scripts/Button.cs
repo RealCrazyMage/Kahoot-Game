ff62a2f2fe693719ef34c495569a97cda9232f07
RealCrazyMage <50674427+RealCrazyMage@users.noreply.github.com>
2020-10-16 22:33:19 -0600

    Added in a scene

---
+++ Kahoot_Game/Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    /*
+     * This is the object that contains button information
+     * 
+     */
+    private bool is_correct;
+
+     Button(bool is_correct)
+    {
+        this.is_correct = is_correct;
+    }
+
+    Button()
+    {
+        is_correct = false;
+    }
+
+
+
+}
---
+++ Kahoot_Game/Assets/Scripts/Choice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choice : MonoBehaviour
+{
+
+    public bool is_correct;
+    public void Click()
+    {
+
+    }
+}
